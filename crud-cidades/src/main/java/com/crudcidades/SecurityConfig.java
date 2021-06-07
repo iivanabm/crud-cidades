4027cbf180718e2ae5585eeb25f12ca6d3cadc4a
iivanabm <ivanammarinho@hotmail.com>
2021-06-06 21:59:09 -0300

    Autenticação, autorização, login e logout

---
+++ crud-cidades/src/main/java/com/crudcidades/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.crudcidades;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/").hasAnyAuthority("listar", "admin")
+				.antMatchers("/criar").hasAuthority("admin")
+				.antMatchers("/excluir").hasAnyAuthority("admin")
+				.antMatchers("/preparaAlterar").hasAuthority("admin")
+				.antMatchers("/alterar").hasAuthority("admin")
+				.anyRequest().denyAll()
+			.and()
+				.formLogin()
+				.loginPage("/login.html").permitAll()
+			.and()
+				.logout().permitAll();
+	}
+	
+	
+	@Bean
+	public PasswordEncoder cifrador() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	/*@EventListener(ApplicationReadyEvent.class)
+	public void printSenhas() {
+		System.out.println(this.cifrador().encode("test123"));
+	}*/
+	
+}
---
+++ crud-cidades/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `usuario` VALUES (1, 'john', '$2a$10$otov2FBuVlG8ToW47eFHRuAdaY7uFdPA0BmAW9i8oIxoIAl9cGx96');
+INSERT INTO `usuario` VALUES (2, 'anna', '$2a$10$otov2FBuVlG8ToW47eFHRuAdaY7uFdPA0BmAW9i8oIxoIAl9cGx96');
+
+INSERT INTO `usuario_papeis` VALUES (1, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'admin');
